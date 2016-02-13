a974f9b8a29109f6beef34d6ac62d78a9bef22be
Javier Rodriguez <javierorodriguez@gmail.com>
2016-02-13 14:36:51 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# My react kit
+Just a kit for playing with React. It uses Gulp as a task manager and includes:
+
+* Browserify for bundling up the code.
+* Watchify for incremental builds.
+* BrowserSync as a development server.
+* Bootstrap-Sass (jQuery is included also).
+
+## Usage
+Clone it:
+```
+$ git clone
+```
+
+Install it:
+```
+$ npm install
+```
+
+And start the thing:
+```
+$ npm start
+```
---
+++ gulpfile.js
@@ -0,0 +1,108 @@
+'use strict';
+
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var gutil       = require('gulp-util');
+var browserify  = require('browserify');
+var watchify    = require('watchify');
+var babelify    = require('babelify');
+var source      = require('vinyl-source-stream');
+var buffer      = require('vinyl-buffer');
+var sourcemaps  = require('gulp-sourcemaps');
+var sass        = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
+
+var config = {
+  port: 8080,
+  devBaseUrl: 'http://localhost',
+  paths: {
+    entryPoint:   './src/js/main.jsx',
+    jsSources:    './src/js/**/*.jsx',
+    sassSources:  './src/sass/**/*.scss',
+    btstrpFonts:  './node_modules/bootstrap-sass/assets/fonts/bootstrap/**.*',
+    public:       './dist/public'
+  }
+}
+
+gulp.task('serve', function() {
+  browserSync.init({
+    open: false,
+    // logFileChanges: false,
+    server: {
+      baseDir: config.paths.public,
+    }
+  });
+
+  gulp.watch(config.paths.sassSources, ['styles']);
+  gulp.watch(config.paths.public + '/css/*.css').on('change', browserSync.reload);
+  gulp.watch(config.paths.public + '/*.html').on('change', browserSync.reload);
+  gulp.watch(config.paths.public + '/images/*').on('change', browserSync.reload);
+});
+
+
+gulp.task('scripts', function () {
+  var bundler = browserify({
+    entries: [config.paths.entryPoint],
+    extensions: ['.jsx'],
+    cache: {},
+    packageCache: {},
+    debug: true
+  });
+
+  bundler.transform(babelify, {
+    presets: ['es2015', 'react'],
+    only: /src\/js/,
+    sourceMaps: true
+  });
+
+  bundler.plugin(watchify, {
+    ignoreWatch: ['**/node_modules/**'],
+    poll: false
+  });
+
+
+  function bundle() {
+    return bundler
+      .bundle()
+      .on('error', function (err) {
+        gutil.log(gutil.colors.red('Browserify error:'), err.message);
+        this.emit('end');
+      })
+      .pipe(source('bundle.js'))
+      .pipe(buffer())
+      .pipe(sourcemaps.init({loadMaps: true}))
+      .pipe(sourcemaps.write('../maps'))
+      .pipe(gulp.dest(config.paths.public + '/js'))
+      .pipe(browserSync.stream());
+  }
+
+  bundle(); // We have to call bundle() to get `update' events.
+
+  bundler.on('update', function (ids) {
+    bundle();
+
+    ids.forEach(function (id) {
+      gutil.log(gutil.colors.green('Updated:'), id);
+    });
+  });
+});
+
+var autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('styles', function () {
+ return gulp.src(config.paths.sassSources)
+  .pipe(sourcemaps.init())
+  .pipe(autoprefixer(autoprefixerOptions))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(sourcemaps.write('../maps'))
+  .pipe(gulp.dest(config.paths.public + '/css'));
+});
+
+gulp.task('fonts', function() { 
+  return gulp.src(config.paths.btstrpFonts)
+    .pipe(gulp.dest(config.paths.public + '/fonts/bootstrap')); 
+});
+
+gulp.task('default', ['scripts', 'styles', 'fonts', 'serve']);
